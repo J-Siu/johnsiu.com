704ae07ce18c89ede024f49be36f3fb7c145e8e9
John Siu <john.sd.siu@gmail.com>
2019-08-15 19:03:09 -0400

    Extract month and day.

--- front-matter-aliases.zsh
+++ front-matter-aliases.zsh
@@ -8,14 +8,20 @@ for i in $(ls $DIR)
 do
 	DATE=$(front-matter-manipulator values ${DIR}/${i} date | json date)
 	YEAR=$(echo $DATE | cut -d\" -f2 | cut -d- -f1)
+	MON=$(echo $DATE | cut -d\" -f2 | cut -d- -f2)
+	DAY=$(echo $DATE | cut -d\" -f2 | cut -d- -f3 | cut -dT -f1)
+
+#	echo ${DATE} ${YEAR} ${MON} ${DAY}
+
 	if [[ ${YEAR} < 2018 ]]
 	then
-		echo --- ${YEAR} ${i}
+		echo --- ${YEAR}/${MON}/${DAY} ${i}
 		# Print Aliases
 		echo ""
 		echo "aliases:"
 		echo "    - /${i}"
 		echo "    - /index.php/${i}"
+		echo "    - /index.php/${YEAR}/${MON}/${DAY}/${i}"
 		echo ""
 	fi
 done
